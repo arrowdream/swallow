c4385e62a17623a43e48a3e4b4cce0b75fb726cc
arrowdream <1131864584@qq.com>
2017-10-24 10:33:23 +0800

    swallow

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+
+// tab
+	function tabs(tabTit,on,tabCon){
+		$(tabTit).children().click(function(){
+			$(this).addClass(on).siblings().removeClass(on);
+			var index = $(tabTit).children().index(this);
+			$(tabCon).children().eq(index).show().siblings().hide();
+		});
+	};
+	tabs(".tab-menu","active",".tab-content");
+
+});
